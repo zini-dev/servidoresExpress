77187d53956b8c52837a00b72853258e8803991e
Aluno <guilhermerosazini@gmail.com>
2025-10-03 20:20:19 -0300

    Servidor 02 - Produtos

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-// Importa módulo EXPRESS para o nosso JS
 const express = require("express");
 
-// Cria instância do EXPRESS
 const app = express();
 
-// Define número da porta
-const PORT = 8000;
+const PORT = 8081;
 
-// Inicia o servidor
 app.listen(PORT, () => {
-    console.log(`O Servidor de usuários está rodando na porta ${PORT}`);
-});
+    console.log(`O Servidor de Produtos está rodando na porta ${PORT}`)
+})
